e76b6c966956ac6be739d78618c9cfdb99d00003
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-13 13:23:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - using puts
- *
- * Return: always 0
+ * main - using puts function
+ * Return: 0 (success)
  */
-
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
