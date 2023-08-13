b55fbd1f2880c743a4c107d3346b40d4e926a95f
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-13 14:56:15 -0700

    Unix is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main = UNIX is simple
+ *
+ * Return: always 1
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" Dora Korpar, 2015-10-19\n");
+	return (1);
