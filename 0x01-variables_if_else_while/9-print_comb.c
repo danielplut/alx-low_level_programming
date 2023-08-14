2777612d9e4f11bf3c204adef47ec58ea90a75d9
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-14 11:50:55 -0700

    Print combination of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,6 +2,7 @@
 /**
  * main - Possible combinations of single-digit numbers
  *
+ * Return: always 0
  */
 
 int main(void)
@@ -11,17 +12,17 @@ int main(void)
 	for (i = 0; i < 10; i++)
 
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
-		{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+
+		if (i != 9)
+
+		{
+			putchar(',');
+			putchar(' ');
 		}
 
 	}
+	putchar('\n');
 
 	return (0);
-
 }
