8f61df23386cf52c3357bc518cf5983f91ab432c
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-13 20:40:38 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	if (l > 5)
 		printf("Last digit of %d is %d and is greater than 5", n, l);
 	else if (l == 0)
-		printf("Last digit of %d is %d and is zero", n, l);
+		printf("Last digit of %d is %d and is 0", n, l);
 	else if (l < 6 && l != 0)
 		printf("Last digit of %d is %d and is less than 6 not zero", n, l);
 
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program to print alphabet in lowercase
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+}
