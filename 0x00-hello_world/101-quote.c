3d9a4337b85b1dae76dd776af83abbe274efefdf
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-13 19:37:15 -0700

    Unix is basically simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main = UNIX is simple
- *
+ * main - program to print standard error
  * Return: 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
