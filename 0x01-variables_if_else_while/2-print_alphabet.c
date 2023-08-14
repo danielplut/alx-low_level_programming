1f904a40c6ce284268080a939e764a527a60f994
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-14 07:17:06 -0700

    Last digit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
  * main - program to print alphabet in lowercase
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
