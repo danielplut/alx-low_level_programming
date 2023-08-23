d9b9f8e0084842079a252f17df97b864be9dd576
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-08-22 20:21:03 -0700

    Reset n to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * reset_to_98 - resets n to 98
- * @n:- Updates pinter to 98
+ * @n: Updates pinter to 98
  * Return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 
 {
 	*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#define MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -13,19 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                                  11,1          All
-
-
 
 #endif
