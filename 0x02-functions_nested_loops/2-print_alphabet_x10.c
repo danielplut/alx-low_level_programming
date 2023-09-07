c44bc03dec1ab2303d4a27226fc8caa5ac45955b
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-09-06 17:04:47 -0700

    Function that prints 10x the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - priint 10x the alphabet
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	j = 0;
+
+	while (j < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}
+
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * int _islower(int c) - Function that checks for lowercase
+ *
+ * Return: 0
+ */
+
+
