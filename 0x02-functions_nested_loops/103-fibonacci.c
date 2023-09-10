fee253ad4e5e569be85460754409d2cbedb23506
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-09-09 19:04:20 -0700

    Program that computes Fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1; k = 2, sum = k;
+	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
 	{
@@ -15,6 +15,7 @@ int main(void)
 		if (k % 2 == 0)
 			sum += k;
 		j = k - j;
+		++i;
 	}
 	printf("%ld\n", sum);
 	return (0);
