d882199829a1fade926b0b91aff2fd1071f25ac1
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-09-09 18:54:26 -0700

    Program that computes Fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - sum of the eve-valued terms of the first 10 terms of Fibonacci
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1; k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+	}
+	printf("%ld\n", sum);
+	return (0);
+
+}
