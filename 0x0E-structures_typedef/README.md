86ebb25d75a74bbc764df34b5462229a00853f04
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-09-12 13:24:56 -0700

    Struct for dog information

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structuresn typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that gives dog information
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
