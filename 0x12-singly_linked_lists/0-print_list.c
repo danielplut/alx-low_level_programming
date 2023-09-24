bc390334c04318d6cb6e1048b4e64c610a292e8a
Daniel Selasi Tudji <danielpluto20@gmail.com>
2023-09-23 19:49:55 -0700

    List of protypes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_lists - function that prints all the elements of a list
+ * @str: sring
+ * @len: length of string
+ * @next: pointer to the next node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+/**
+ * struct list_s - singly listed list
+ * @str: string
+ * @len: length of string
+ * @next: points to the next node
+ * Description: singly linked lists
+ */
+typedef struct list_s
+{
+	cahr *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
- _putchar - writes the character c to stdout;
- size_t print_list(const list_t *h);
- list_t *add_node(list_t **head, const char *str);
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
